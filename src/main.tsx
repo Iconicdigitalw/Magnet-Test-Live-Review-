0f3c2507a2bebb8b3a2160dd4ed833c5c3ea40ea
iconicdigitalw <info@iconicdigitalworld.com>
2025-09-22 23:38:03 +0000

    404 error on questionnaire editing fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 /* import { TempoDevtools } from 'tempo-devtools'; [deprecated] */
-/* TempoDevtools.init() [deprecated] */;
-
-const basename = import.meta.env.BASE_URL;
+/* TempoDevtools.init() [deprecated] */ const basename = import.meta.env
+  .BASE_URL;
+const Router =
+  import.meta.env.MODE === "development" ? BrowserRouter : HashRouter;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <Router basename={basename}>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
 );
